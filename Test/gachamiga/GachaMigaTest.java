992bd97a7e2494372b704c12858060e05bbd9af4
Ramon2122 <monjr2002@gmail.com>
2023-05-25 17:32:30 +0200

    Ćodigo refactorizado V2 (he simplificado el código usando el método Integer.toString para convertir el número entero o una cadena de texto. También he añadido el método main para que imprima los números del 1 al 100 y el método convert para imprimir gacha, miga o gachamiga cuando corresponda.) También he realizado el test 1

---
+++ Test/gachamiga/GachaMigaTest.java
@@ -0,0 +1,17 @@
+package gachamiga;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class GachaMigaTest {
+    public class GachamigueroTest {
+
+        @Test
+        public void test1() {
+            assertEquals("1", GachaMiga.convert(1));
+        }
+
+
+
+    }
+}
--- src/gachamiga/GachaMigaV2.java
+++ src/gachamiga/GachaMigaV2.java
@@ -1,2 +1,23 @@
+package gachamiga;
+
+public class GachaMigaV2  {
+    public static String convert(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "Gachamiga";
+        } else if (number % 3 == 0) {
+            return "Gacha";
+        } else if (number % 5 == 0) {
+            return "Miga";
+        } else {
+            return Integer.toString(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(convert(i));
+        }
+    }
+}
 
 
